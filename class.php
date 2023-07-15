7445f474d6057a2cddd5fd1ec4989ed13f2b52e3
Ashish kumar mishra <113705926+ashish-kumar-mishra29@users.noreply.github.com>
2023-07-15 18:03:16 +0530

    Add files via upload

---
+++ class.php
@@ -0,0 +1,27 @@
+<html>
+    <body>
+        <h1>The fruit program</h1>
+        <?php
+            class Fruit {
+                public $name;
+                public $color;
+
+                function set_name($name) {
+                    $this->name = $name;
+                }
+
+                function get_name() {
+                    return $this->name;
+                }
+            }
+            $apple = new Fruit();
+            $banana = new Fruit();
+            $apple->set_name('Apple');
+            $banana->set_name('Banana');
+
+            echo $apple->get_name();
+            echo $banana->get_name();
+            
+        ?>
+    </body>
+</html>
---
+++ tutorial.php
@@ -0,0 +1,66 @@
+<?php
+// script goes here
+    //echo "Hello world" ;
+    //for declaration use $sign
+    /*$str = "Welcome to Marwadi University";
+    $num = 5;
+    $float = 8.5;
+
+    echo "<h1>" .$str."</h1>";
+    echo "<h2>" .$num."</h2>";
+    echo "<h3>" .$float."</h3>";*/
+
+    // array
+    //$laptop_brands = array("HP", "Lenovo" , "Asus" , "Dell" , "MSI");
+    //echo var_dump($laptop_brands);
+    //echo var_dump($laptop_brands[1]);
+
+    //consonants are like variable but they are constants
+    //define("laptops", ["Hp" , "Lenovo" , "Asus"]);
+    //echo laptops[1];
+
+    //Conditional statements
+    /*$var = 1;
+    if($var > 7){
+        echo $var;
+    }
+    else {
+        echo "The condition did not meet";
+    }*/
+
+    /*$var = 9;
+    if($var % 2 == 0 && $var % 3 == 0){
+        echo "Divisible by both 2 and 3";
+    }
+    elseif($var % 2 == 0) {
+        echo "Divisible by 2";
+    }else {
+        echo "Divisible by 3";
+    }*/
+
+    //switch statement
+    //$level =3;
+    /*switch ($level){
+        case 1:
+            echo "You are playing at easy level";
+        break;
+        case 2:
+            echo "You are playing at medium level";
+        break;
+        case 3:
+            echo "You are playing at Hard level";
+        break;
+        case 4:
+            echo "You are playing at very hard level";
+        break;
+        default:
+        echo "Invalid Input";
+    }*/
+
+    /*$var = 2;
+    while ($var <=10 ){
+        echo "Numbers: $var <br>";
+        $var++; 
+    }*/
+
+?>
